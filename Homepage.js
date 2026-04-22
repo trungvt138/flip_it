23bc4082aabd0058e932e725a806f630ad5d56b7
trung <trungvt138@github.com>
2026-04-22 17:59:24 +0200

    implementing navbar

--- Homepage.js
+++ Homepage.js
@@ -1,14 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Image } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Homepage() {
   return (
-    <View style={styles.container}>
-      <TextInput style={styles.searchBox} placeholder="Search..." />
-      <View style={styles.horizontalRuler} />
-      {/* <Text style={styles.text}>Welcome to FlipIt!</Text> */}
-      {/* <StatusBar style="auto" /> */}
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+
+        <TextInput style={styles.searchBox} placeholder="Search..." />
+        <View style={styles.horizontalRuler} />
+        {/* <Text style={styles.text}>Welcome to FlipIt!</Text> */}
+        {/* <StatusBar style="auto" /> */}
+        <View style={styles.plusCircleIcon}>
+          <Image source={require("./assets/plus-circle.png")} />
+          <Text style={styles.iconInstructText}>
+            Tap to create your first card set
+          </Text>
+        </View>
+
+        <View style={styles.navBar}>
+          <TouchableOpacity>
+            <Image source={require("./assets/home-navbar.png")} />
+            <Text>Home</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require("./assets/home-navbar.png")} />
+            <Text>Create</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require("./assets/home-navbar.png")} />
+            <Text>Library</Text>
+          </TouchableOpacity>
+        </View>
+
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -17,27 +44,36 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'flex-start',
-    paddingTop: 32,
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    paddingTop: 8,
+    alignItems: 'center'
   },
   searchBox: {
     alignSelf: 'stretch',
     height: 36,
-    alignItems: 'center',
     backgroundColor: '#D9D9D9',
     marginHorizontal: 30,
     borderRadius: 8,
     paddingLeft: 13,
     paddingTop: 10,
-    paddingBottom: 9,
     textAlignVertical: 'center',
   },
   horizontalRuler: {
+    alignSelf: 'stretch',
     borderBottomColor: 'black',
     borderBottomWidth: StyleSheet.hairlineWidth,
-    paddingTop: 28
+    paddingTop: 15
+  },
+  plusCircleIcon: {
+    paddingTop: 212,
+    alignItems: 'center'
+  },
+  navBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: 360,
+    height: 75,
+    backgroundColor: '#D9D9D9',
+    top: 250
   }
 });
