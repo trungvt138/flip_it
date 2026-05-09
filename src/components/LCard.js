cc21127ad02b184fe5479cdbaf9a8d9c9c9cd5db
Đàm Quang Trung <trungvt138@github.com>
2026-05-09 21:48:00 +0200

    finish implementing Practice screen

--- src/components/LCard.js
+++ src/components/LCard.js
@@ -1,35 +1,129 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { useRef, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image, Animated } from "react-native";
 
-export default function LCard() {
+const LCard = () => {
+    const flipAnim = useRef(new Animated.Value(0)).current;
+    const [flipped, setFlipped] = useState(false);
+
+    const frontRotate = flipAnim.interpolate({
+        inputRange: [0, 180],
+        outputRange: ['0deg', '180deg'],
+    });
+
+    const backRotate = flipAnim.interpolate({
+        inputRange: [0, 180],
+        outputRange: ['180deg', '360deg'],
+    });
+
+    const flipToFrontStyle = {
+        transform: [{ perspective: 1000 }, { rotateY: frontRotate }],
+    };
+
+    const flipToBackStyle = {
+        transform: [{ perspective: 1000 }, { rotateY: backRotate }],
+    };
+
+    const flip = () => {
+        if (flipped) {
+            Animated.spring(flipAnim, {
+                toValue: 0,
+                friction: 8,
+                tension: 10,
+                useNativeDriver: true,
+            }).start();
+        } else {
+            Animated.spring(flipAnim, {
+                toValue: 180,
+                friction: 8,
+                tension: 10,
+                useNativeDriver: true,
+            }).start();
+        }
+        setFlipped(!flipped);
+    };
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>What is HTML?</Text>
-            <TouchableOpacity style={styles.icon}>
-                <Image source={require('../../assets/front-flip.png')}></Image>
-            </TouchableOpacity>
+        <View>
+            <Animated.View style={[styles.card, styles.front, flipToFrontStyle]}>
+                <View></View>
+                <Text style={styles.text}>What is HTML?</Text>
+                <TouchableOpacity style={styles.icon} onPress={flip}>
+                    <Image source={require('../../assets/front-flip.png')}></Image>
+                </TouchableOpacity>
+            </Animated.View>
+            <Animated.View style={[styles.card, styles.back, flipToBackStyle]}>
+                <View></View>
+                <Text style={styles.text}>Hyper Text Markup Language</Text>
+                <TouchableOpacity style={styles.icon} onPress={flip}>
+                    <Image source={require('../../assets/back-flip.png')}></Image>
+                </TouchableOpacity>
+            </Animated.View>
+
+            {flipped && (
+                <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.repeat}>
+                    <Text style={{color: 'white', fontSize: 18, fontWeight: 'bold', alignSelf: 'center', marginTop: 40}}>Repeat</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.easy}>
+                    <Text style={{color: 'white', fontSize: 18, fontWeight: 'bold', alignSelf: 'center', marginTop: 40}}>Easy</Text>
+                </TouchableOpacity>
+            </View>
+            )}
+            
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    card: {
         width: 300,
         height: 377,
         borderRadius: 14,
         backgroundColor : '#D9D9D9',
-        shadowColor: 'black',
-        shadowOffset: {width: 0, height: 4},
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 4,
-        justifyContent: 'center',
-        
+        // shadowColor: 'black',
+        // shadowOffset: {width: 0, height: 4},
+        // shadowOpacity: 0.25,
+        // shadowRadius: 4,
+        // elevation: 4,
+        justifyContent: 'space-between',
+        marginTop: 67,
+        alignItems: 'center',
+        backfaceVisibility: 'hidden',
     },
     text: {
-        alignSelf: 'center',
+        textAlign: 'center',
+        fontSize: 24,
+        fontWeight: 'bold',
     },
     icon: {
         alignSelf: 'center',
-        
+        marginTop: 20,
+        paddingBottom: 15,
+    },
+    front: {
+        backgroundColor : '#D9D9D9',
+    },
+    back: {
+        position: 'absolute',
+        backgroundColor : '#9080F7',
+    },
+    buttonContainer: {
+        marginTop: 31,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    repeat: {
+        width: 133,
+        height: 112,
+        backgroundColor: '#EC221F',
+        borderRadius: 14,
+    },
+    easy: {
+        width: 133,
+        height: 112,
+        backgroundColor: '#34C759',
+        borderRadius: 14,
     }
 })
+
+export default LCard;
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -4,8 +4,8 @@ export default function ProgressBar() {
     return (
         <View style={styles.container}>
             <View style={styles.progressBar}>
-                <View style={{ flex: 3, backgroundColor: '#9080F7'}}></View>
-                <View style={{ flex: 7, backgroundColor: '#D9D9D9'}}></View>
+                <View style={{ flex: 3, backgroundColor: '#9080F7', borderTopLeftRadius: 20, borderBottomLeftRadius: 20}}></View>
+                <View style={{ flex: 7, backgroundColor: '#D9D9D9', borderTopRightRadius: 20, borderBottomRightRadius: 20}}></View>
                 
             </View>
             <Text style={{alignSelf: 'center', marginTop: 10}}>3/10</Text>
@@ -14,19 +14,17 @@ export default function ProgressBar() {
 }
 
 const styles = StyleSheet.create({
-    progressBar: {
-        height: 10,
-        
-        flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'center',
-        
-    },
-
     container: {
         flexDirection: 'column',
         paddingHorizontal: 30,
         justifyContent: 'center',
         marginTop: 31,
-    }
+    },
+    progressBar: {
+        height: 10,
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'center',
+    },
+    
 })
