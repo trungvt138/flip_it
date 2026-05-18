bcbc2380f66d5fcd34402ffe89fa22f038e06460
Đàm Quang Trung <trungvt138@github.com>
2026-05-18 17:39:57 +0200

    add function x icon in practice

--- src/screens/Library.js
+++ src/screens/Library.js
@@ -1,57 +1,63 @@
-
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import TextBox from '../components/TextBox';
-import HorizontalRuler from '../components/HorizontalRuler';
-import Navbar from '../components/Navbar'
-import LearningBox from '../components/LearningBox';
-import { useLearningBoxes } from '../hooks/useLearningBoxes';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import TextBox from "../components/TextBox";
+import HorizontalRuler from "../components/HorizontalRuler";
+import Navbar from "../components/Navbar";
+import LearningBox from "../components/LearningBox";
+import { useLearningBoxes } from "../hooks/useLearningBoxes";
 
 export default function Library() {
-    const { learningCards, addLearningBox, deleteLearningBox } = useLearningBoxes();
-    return (
-        <SafeAreaProvider>
-            <SafeAreaView style={styles.container}>
-                <View style={styles.content}>
-                    <TextBox style={styles.textBox} placeholder="Search..." />
-                    <HorizontalRuler />
-                    <Text style={styles.sortText}> Sort by: Newest first </Text>
-                    
-                    <ScrollView style={{width: '100%'}} contentContainerStyle={{alignItems: 'center', gap: 38}}>
-                        {learningCards.map((card, index) => (
-                            <LearningBox key={index} card={card} onDelete={() => deleteLearningBox(index)} />
-                        ))}
-                    </ScrollView>
-                </View>
-                <Navbar />
-            </SafeAreaView>
-        </SafeAreaProvider>
-    );
+  const { learningCards, addLearningBox, deleteLearningBox } = useLearningBoxes();
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.content}>
+          <TextBox style={styles.textBox} placeholder="Search..." />
+          <HorizontalRuler />
+          <Text style={styles.sortText}> Sort by: Newest first </Text>
+
+          <ScrollView
+            style={{ width: "100%" }}
+            contentContainerStyle={{ alignItems: "center", gap: 38 }}
+          >
+            {learningCards.map((card, index) => (
+              <LearningBox
+                key={index}
+                card={card}
+                onDelete={() => deleteLearningBox(index)}
+              />
+            ))}
+          </ScrollView>
+        </View>
+        <Navbar />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        paddingTop: 8,
-        alignItems: "center",
-    },
-    content: {
-        flex: 1,
-        width: "100%",
-        alignItems: "center",
-    },
-    textBox: {
-    marginHorizontal: 30
-    },
-    sortText: {
-        marginLeft: 30,
-        marginTop: 12,
-        width: 117,
-        height: 22,
-        fontSize: 12,
-        color: 'black',
-        alignSelf: 'flex-start',
-        marginBottom: 15,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 8,
+    alignItems: "center",
+  },
+  content: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+  },
+  textBox: {
+    marginHorizontal: 30,
+  },
+  sortText: {
+    marginLeft: 30,
+    marginTop: 12,
+    width: 117,
+    height: 22,
+    fontSize: 12,
+    color: "black",
+    alignSelf: "flex-start",
+    marginBottom: 15,
+  },
 });
--- src/screens/Practice.js
+++ src/screens/Practice.js
@@ -2,22 +2,26 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet, Image, Text, View, TextInput, TouchableOpacity } from "react-native";
 import ProgressBar from "../components/ProgressBar";
 import LCard from "../components/LCard";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Practice() {
+  const navigation = useNavigation();
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <View style={styles.head}>
-            <Text style={styles.screenTitle}>Web Programming</Text>
-            <TouchableOpacity style={styles.closeButton}>
-              <Image source={require("../../assets/X.png")} />
-            </TouchableOpacity>
+          <Text style={styles.screenTitle}>Web Programming</Text>
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => navigation.goBack()}
+          >
+            <Image source={require("../../assets/X.png")} />
+          </TouchableOpacity>
         </View>
-        
+
         <ProgressBar />
 
         <LCard />
-                
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -32,7 +36,7 @@ const styles = StyleSheet.create({
   },
   head: {
     flexDirection: "row",
-    justifyContent: 'space-around',
+    justifyContent: "space-around",
     width: "100%",
     alignItems: "center",
     paddingHorizontal: 20,
@@ -43,7 +47,5 @@ const styles = StyleSheet.create({
     flex: 1,
     maxWidth: "90%",
   },
-  closeButton: {
-    
-  },
+  closeButton: {},
 });
